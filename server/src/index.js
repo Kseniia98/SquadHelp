abea6f9a3ada007c6ce5900d81e2d11610868254
Kseniia98 <kseniia.plakhotnia@gmail.com>
2022-07-08 21:48:53 +0300

    Add startup changes

--- server/src/index.js
+++ server/src/index.js
@@ -6,12 +6,19 @@ const router = require('./router');
 const controller = require('./socketInit');
 const handlerError = require('./handlerError/handler');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/public', express.static('public'));
+app.use((req, res, next) => {
+  console.log('new request', req.method, req.path);
+
+  next();
+});
+
 app.use(router);
 app.use(handlerError);
 
